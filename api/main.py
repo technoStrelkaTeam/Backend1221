d8645c0c886d4c5cf59aac7c1f3ba3cacbcb7c9a
jiko-star <ilya_skokov@internet.ru>
2026-04-28 14:00:12 +0300

    add network CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from api.database import create_db_and_tables
 from api.routers import user
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user.router)
 
 @app.get("/test")
